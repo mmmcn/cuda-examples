de3e36ac7d64549f630a68061d3c9f482f215e53
mmmcn <1139553802@qq.com>
2024-07-20 19:41:48 +0800

    add more kernels of reduce_sum and performance data is supplemented

--- reduce/README.md
+++ reduce/README.md
@@ -1,10 +1,33 @@
+- reduce v1(reduceSumGlobalMemKernel): baseline，直接基于global memory
+- reduce v2(reduceSumSharedMemKernel): 在shared memory中做reduce
+- reduce v3(reduceSumSharedMemLessWarpDivergenceKernel): 减少warp divergence情况
+- reduce v4(reduceSumSharedMemNoBankConflictKernel): 优化shared memory方式，避免产生bank conflict
+- reduce v5(reduceSumAddDuringLoadKernel): 每个block会把相邻blockDim.x个元素加到当前第threadIdx.x个shared memory上，即所有的线程其实都参与了Add计算
+- reduce v6(reduceSumAddDuringLoadWarpReduceKernel): 最后一个warp避免`__syncthreads`，
+- reduce v7(reduceSumAddDuringLoadWarpReduceUnrollLoopKernel): 把kernel中的循环全部展开，减少一些指令
+- reduce v8(reduceSumMultiAddKernel): 每个block会把相邻NUM_PER_THREAD * blockDim.x个元素加到当前第threadIdx.x个shared memory上 （固定了block数，方便调整block来查看不同block对性能的影响，但实验中取256、512、1024并没发现明显区别）
+- reduce v9(blockReduceSumKernel): 利用了`__shfl_down_sync`，由于只有每个block中的第一个warp会用到shared memory，不用考虑bank conflict
 
 在4060显卡上基于Nsight Compute测试得到的性能如下：
-|reduce kernel|cost(us)|Bandwidth [%] |
-| --- | --- | --- |
-|reduce v1| 3091.2 | 33.17% |
-|reduce v2| 2745.4 | 22.53% |
-|reduce v3| 1807.7 | 29.15% |
-|reduce v4| 1740.0 | 34.68% |
-|reduce v5| 898.1 | 58.56% |
-|reduce v6| 554.6 | 96.38% |
+|reduce kernel|cost(us)| Bandwidth [%] | DRAM Throughput [%] |
+| --- | --- | --- | --- |
+|reduce v1| 6196.16 | 15.93% | 33.37% |
+|reduce v2| 5476.86 | 18.02% | 19.89% |
+|reduce v3| 3610.98 | 27.33% | 29.18% |
+|reduce v4| 3467.17 | 28.46% | 31.28% |
+|reduce v5| 1792.64 | 55.05% | 58.68% |
+|reduce v6| 1098.59 | 89.83% | 96.56% |
+|reduce v7| 1086.59 | 90.82% | 97.01% |
+|reduce v8| 1088.26 | 90.69% | 97.22% |
+|reduce v9| 1128.10 | 87.48% | 97.40% |
+
+发现最终kernel带宽利用率能到97.40%，还是挺惊讶的，不过这个数据是直接看的Nsight Compute的DRAM Throughput，如果按照`64 * 1024 * 1024 * 4 / 1128100 / 272`来算的话是87.48%
+
+v6~v9性能可能存在波动，以后有机会再测试下
+
+--------------
+相关资料
+- [深入浅出GPU优化系列：reduce优化](https://zhuanlan.zhihu.com/p/426978026)
+- [reduce优化学习笔记](https://zhuanlan.zhihu.com/p/596012674)
+- [Optimizing Parallel Reduction in CUDA](https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf)
+- [PyTorch-BlockReduceSum](https://github.com/pytorch/pytorch/blob/8ac9b20d4b090c213799e81acf48a55ea8d437d6/aten/src/ATen/native/cuda/block_reduce.cuh#L58)
--- reduce/reduce_sum.cu
+++ reduce/reduce_sum.cu
@@ -11,6 +11,7 @@
 #define NUM_WARMUP 2
 #define NUM_REPEAT 3
 #define NUM_THREADS 256
+#define WARP_SIZE 32
 
 template <typename T>
 inline void checkResult(T actual, T desired) {
@@ -170,18 +171,40 @@ __global__ void reduceSumAddDuringLoadKernel(T* d_out, T* d_in, int64_t numel) {
     }
 }
 
+template <typename T, uint32_t BLOCK_SIZE>
+__device__ void warpReduceUnroll(volatile T* cache, uint32_t tid) {
+    if constexpr (BLOCK_SIZE >= 64) {
+        cache[tid] += cache[tid + 32];
+    }
+    if constexpr (BLOCK_SIZE >= 32) {
+        cache[tid] += cache[tid + 16];
+    }
+    if constexpr (BLOCK_SIZE >= 16) {
+        cache[tid] += cache[tid + 8];
+    }
+    if constexpr (BLOCK_SIZE >= 8) {
+        cache[tid] += cache[tid + 4];
+    }
+    if constexpr (BLOCK_SIZE >= 4) {
+        cache[tid] += cache[tid + 2];
+    }
+    if constexpr (BLOCK_SIZE >= 2) {
+        cache[tid] += cache[tid + 1];
+    }
+}
+
 template <typename T>
-__device__ void warpReduce(volatile T* cache, int tid){
-    cache[tid] += cache[tid+32];
-    cache[tid] += cache[tid+16];
-    cache[tid] += cache[tid+8];
-    cache[tid] += cache[tid+4];
-    cache[tid] += cache[tid+2];
-    cache[tid] += cache[tid+1];
+__device__ void warpReduce(volatile T* cache, uint32_t tid) {
+    cache[tid] += cache[tid + 32];
+    cache[tid] += cache[tid + 16];
+    cache[tid] += cache[tid + 8];
+    cache[tid] += cache[tid + 4];
+    cache[tid] += cache[tid + 2];
+    cache[tid] += cache[tid + 1];
 }
 
 template <bool IS_LAST = false, typename T>
-__global__ void reduceSumAddDuringLoadOptKernel(T* d_out, T* d_in, int64_t numel) {
+__global__ void reduceSumAddDuringLoadWarpReduceKernel(T* d_out, T* d_in, int64_t numel) {
     extern __shared__ T shm[];
     uint32_t tid = threadIdx.x;
     uint32_t gid;
@@ -205,6 +228,145 @@ __global__ void reduceSumAddDuringLoadOptKernel(T* d_out, T* d_in, int64_t numel
     if (tid < 32) warpReduce(shm, tid);
     if (tid == 0) {
         d_out[blockIdx.x] = shm[tid];
+    }
+}
+
+template <uint32_t BLOCK_SIZE = 256, bool IS_LAST = false, typename T>
+__global__ void reduceSumAddDuringLoadWarpReduceUnrollLoopKernel(T* d_out, T* d_in, int64_t numel) {
+    extern __shared__ T shm[];
+    uint32_t tid = threadIdx.x;
+    uint32_t gid;
+    if constexpr (IS_LAST) {
+        gid = threadIdx.x + blockIdx.x * blockDim.x;
+        shm[tid] = d_in[gid];
+    } else {
+        gid = threadIdx.x + blockIdx.x * (blockDim.x * 2);
+        shm[tid] = d_in[gid] + d_in[gid + blockDim.x];
+    }
+    
+    __syncthreads();
+
+    if constexpr (BLOCK_SIZE >= 512) {
+        if (tid < 256) {
+            shm[tid] += shm[tid + 256];
+        }
+        __syncthreads();
+    }
+    if constexpr (BLOCK_SIZE >= 256) {
+        if (tid < 128) {
+            shm[tid] += shm[tid + 128];
+        }
+        __syncthreads();
+    }
+    if constexpr (BLOCK_SIZE >= 128) {
+        if (tid < 64) {
+            shm[tid] += shm[tid + 64];
+        }
+        __syncthreads();
+    }
+
+    if (tid < 32) warpReduceUnroll<T, BLOCK_SIZE>(shm, tid);
+    if (tid == 0) {
+        d_out[blockIdx.x] = shm[tid];
+    }
+}
+
+template <uint32_t BLOCK_SIZE, uint32_t NUM_PER_THREAD, typename T>
+__global__ void reduceSumMultiAddKernel(T* d_out, T* d_in, int64_t numel) {
+    extern __shared__ T shm[];
+    uint32_t tid = threadIdx.x;
+    uint32_t gid = threadIdx.x + blockIdx.x * (BLOCK_SIZE * NUM_PER_THREAD);
+
+    // each threads load and accumulate NUM_PER_THREAD element from global mem to shared mem
+    shm[tid] = 0;
+    #pragma unroll
+    for (int i = 0; i < NUM_PER_THREAD; i++) {
+        shm[tid] += d_in[i * BLOCK_SIZE + gid];
+    }
+    __syncthreads();
+
+    
+    // BLOCK_SIZE wont greater than 512 actually
+    if constexpr (BLOCK_SIZE >= 512) {
+        if (tid < 256) {
+            shm[tid] += shm[tid + 256];
+        }
+        __syncthreads();
+    }
+    if constexpr (BLOCK_SIZE >= 256) {
+        if (tid < 128) {
+            shm[tid] += shm[tid + 128];
+        }
+        __syncthreads();
+    }
+    if constexpr (BLOCK_SIZE >= 128) {
+        if (tid < 64) {
+            shm[tid] += shm[tid + 64];
+        }
+        __syncthreads();
+    }
+
+    if (tid < 32) {
+        warpReduceUnroll<T, BLOCK_SIZE>(shm, tid);
+    }
+    if (tid == 0) {
+        d_out[blockIdx.x] = shm[tid];
+    }
+}
+
+
+template <typename T, uint32_t BLOCK_SIZE>
+__device__ T warpReduceSHFL(T val) {
+    // the BLOCK_SIZE alway even number, do not warry
+    if constexpr (BLOCK_SIZE >= 32) {
+        val += __shfl_down_sync(0xffffffff, val, 16);
+    }
+    if constexpr (BLOCK_SIZE >= 16) {
+       val += __shfl_down_sync(0xffffffff, val, 8);
+    }
+    if constexpr (BLOCK_SIZE >= 8) {
+        val += __shfl_down_sync(0xffffffff, val, 4);
+    }
+    if constexpr (BLOCK_SIZE >= 4) {
+        val += __shfl_down_sync(0xffffffff, val, 2);
+    }
+    if constexpr (BLOCK_SIZE >= 2) {
+        val += __shfl_down_sync(0xffffffff, val, 1);
+    }
+    return val;
+}
+
+template <uint32_t BLOCK_SIZE, uint32_t NUM_PER_THREAD, typename T>
+__global__ void blockReduceSumKernel(T* d_out, T* d_in, int64_t numel) {
+    T sum = static_cast<T>(0.0);
+    uint32_t tid = threadIdx.x;
+    uint32_t gid = threadIdx.x + blockIdx.x * (BLOCK_SIZE * NUM_PER_THREAD);
+
+    // each threads load and accumulate NUM_PER_THREAD element from global mem to register
+    #pragma unroll
+    for (int i = 0; i < NUM_PER_THREAD; i++) {
+        sum += d_in[i * BLOCK_SIZE + gid];
+    }
+    
+    static __shared__ T shm[WARP_SIZE];  // warp level shared memory
+    uint32_t lid = tid % WARP_SIZE;      // Lane index
+    uint32_t wid = tid / WARP_SIZE;      // Warp index
+
+    sum = warpReduceSHFL<T, BLOCK_SIZE>(sum);
+
+    if (lid == 0) {
+        shm[wid] = sum;
+    }
+    __syncthreads();
+
+    // read from shared memory only if that warp exists
+    sum = (tid < BLOCK_SIZE / WARP_SIZE) ? shm[lid] : static_cast<T>(0.0);
+    // do last reduce using 1-th warp
+    if (wid == 0) {
+        sum = warpReduceSHFL<T, BLOCK_SIZE / WARP_SIZE>(sum);
+    }
+    if (tid == 0) {
+        d_out[blockIdx.x] = sum;
     }
 }
 
@@ -438,7 +600,127 @@ void runReduceSumV6() {
 
     for (int i = 0; i < NUM_REPEAT; i++) {
         CHECK_CUDA(cudaMemcpy(device_in, host_in, sizeof(T) * NUMEL, cudaMemcpyHostToDevice));
-        reduceSumAddDuringLoadOptKernel<<<num_blocks, num_threads, shm_size>>>(device_result, device_in, NUMEL);
+        reduceSumAddDuringLoadWarpReduceKernel<<<num_blocks, num_threads, shm_size>>>(device_result, device_in, NUMEL);
+    }
+    CHECK_CUDA(cudaMemcpy(host_actual_result, device_result, num_blocks * sizeof(T), cudaMemcpyDeviceToHost));
+    checkResult(host_actual_result, host_golden_result, num_blocks);
+
+    free(host_in);
+    free(host_actual_result);
+    free(host_golden_result);
+    cudaFree(device_in);
+    cudaFree(device_result);
+}
+
+template <typename T, int64_t NUMEL>
+void runReduceSumV7() {
+    constexpr uint32_t num_threads = NUM_THREADS;
+    constexpr uint32_t num_blocks = (NUMEL + 2 * num_threads - 1) / (2 * num_threads);
+    static_assert(NUMEL % num_threads == 0, "For current implementation, the numel should be divisible by num_threads");
+
+    T* host_in = (T*)malloc(sizeof(T) * NUMEL);
+    T* host_golden_result = (T*)malloc(sizeof(T) * num_blocks);
+    T* host_actual_result = (T*)malloc(sizeof(T) * num_blocks);
+
+    randomInit(host_in, NUMEL);
+    for (int i = 0; i < num_blocks; i++) {
+        T sum = static_cast<T>(0.0);
+        for (int j = 0; j < num_threads * 2; j++) {
+            sum += host_in[i * num_threads * 2 + j];
+        }
+        host_golden_result[i] = sum;
+    }
+
+    T* device_in;
+    T* device_result;
+    CHECK_CUDA(cudaMalloc((void **)&device_in, sizeof(T) * NUMEL));
+    CHECK_CUDA(cudaMalloc((void **)&device_result, num_blocks * sizeof(T)));
+    uint32_t shm_size = sizeof(T) * num_threads;
+
+    for (int i = 0; i < NUM_REPEAT; i++) {
+        CHECK_CUDA(cudaMemcpy(device_in, host_in, sizeof(T) * NUMEL, cudaMemcpyHostToDevice));
+        reduceSumAddDuringLoadWarpReduceUnrollLoopKernel<<<num_blocks, num_threads, shm_size>>>(device_result, device_in, NUMEL);
+    }
+    CHECK_CUDA(cudaMemcpy(host_actual_result, device_result, num_blocks * sizeof(T), cudaMemcpyDeviceToHost));
+    checkResult(host_actual_result, host_golden_result, num_blocks);
+
+    free(host_in);
+    free(host_actual_result);
+    free(host_golden_result);
+    cudaFree(device_in);
+    cudaFree(device_result);
+}
+
+template <typename T, int64_t NUMEL>
+void runReduceSumV8() {
+    constexpr uint32_t num_threads = NUM_THREADS;
+    constexpr uint32_t num_blocks = 1024;
+    constexpr uint32_t num_per_block = NUMEL / num_blocks;
+    constexpr uint32_t num_per_thread = num_per_block / num_threads;
+    static_assert(NUMEL % num_threads == 0, "For current implementation, the numel should be divisible by num_threads");
+
+    T* host_in = (T*)malloc(sizeof(T) * NUMEL);
+    T* host_golden_result = (T*)malloc(sizeof(T) * num_blocks);
+    T* host_actual_result = (T*)malloc(sizeof(T) * num_blocks);
+
+    randomInit(host_in, NUMEL);
+    for (int i = 0; i < num_blocks; i++) {
+        T sum = static_cast<T>(0.0);
+        for (int j = 0; j < num_per_block; j++) {
+            sum += host_in[i * num_per_block + j];
+        }
+        host_golden_result[i] = sum;
+    }
+
+    T* device_in;
+    T* device_result;
+    CHECK_CUDA(cudaMalloc((void **)&device_in, sizeof(T) * NUMEL));
+    CHECK_CUDA(cudaMalloc((void **)&device_result, num_blocks * sizeof(T)));
+    uint32_t shm_size = sizeof(T) * num_threads;
+
+    for (int i = 0; i < NUM_REPEAT; i++) {
+        CHECK_CUDA(cudaMemcpy(device_in, host_in, sizeof(T) * NUMEL, cudaMemcpyHostToDevice));
+        reduceSumMultiAddKernel<num_threads, num_per_thread><<<num_blocks, num_threads, shm_size>>>(device_result, device_in, NUMEL);
+    }
+    CHECK_CUDA(cudaMemcpy(host_actual_result, device_result, num_blocks * sizeof(T), cudaMemcpyDeviceToHost));
+    checkResult(host_actual_result, host_golden_result, num_blocks);
+
+    free(host_in);
+    free(host_actual_result);
+    free(host_golden_result);
+    cudaFree(device_in);
+    cudaFree(device_result);
+}
+
+template <typename T, int64_t NUMEL>
+void runReduceSumV9() {
+    constexpr uint32_t num_threads = NUM_THREADS;
+    constexpr uint32_t num_blocks = 1024;
+    constexpr uint32_t num_per_block = NUMEL / num_blocks;
+    constexpr uint32_t num_per_thread = num_per_block / num_threads;
+    static_assert(NUMEL % num_threads == 0, "For current implementation, the numel should be divisible by num_threads");
+
+    T* host_in = (T*)malloc(sizeof(T) * NUMEL);
+    T* host_golden_result = (T*)malloc(sizeof(T) * num_blocks);
+    T* host_actual_result = (T*)malloc(sizeof(T) * num_blocks);
+
+    randomInit(host_in, NUMEL);
+    for (int i = 0; i < num_blocks; i++) {
+        T sum = static_cast<T>(0.0);
+        for (int j = 0; j < num_per_block; j++) {
+            sum += host_in[i * num_per_block + j];
+        }
+        host_golden_result[i] = sum;
+    }
+
+    T* device_in;
+    T* device_result;
+    CHECK_CUDA(cudaMalloc((void **)&device_in, sizeof(T) * NUMEL));
+    CHECK_CUDA(cudaMalloc((void **)&device_result, num_blocks * sizeof(T)));
+
+    for (int i = 0; i < NUM_REPEAT; i++) {
+        CHECK_CUDA(cudaMemcpy(device_in, host_in, sizeof(T) * NUMEL, cudaMemcpyHostToDevice));
+        blockReduceSumKernel<num_threads, num_per_thread><<<num_blocks, num_threads>>>(device_result, device_in, NUMEL);
     }
     CHECK_CUDA(cudaMemcpy(host_actual_result, device_result, num_blocks * sizeof(T), cudaMemcpyDeviceToHost));
     checkResult(host_actual_result, host_golden_result, num_blocks);
@@ -451,7 +733,8 @@ void runReduceSumV6() {
 }
 
 int main(int argc, char* argv[]) {
-    constexpr int64_t numel = 32 * 1024 * 1024;
+    constexpr int64_t numel = 64 * 1024 * 1024;
+    static_assert(numel % NUM_THREADS == 0, "For current implementation, the numel should be divisible by num_threads");
     using scalar_t = float;
 
     if (argc == 1) {
@@ -477,7 +760,15 @@ int main(int argc, char* argv[]) {
             case 6:
                 runReduceSumV6<scalar_t, numel>();  // avoid explict synchronization in warp0
                 break;
-                
+            case 7:
+                runReduceSumV7<scalar_t, numel>();  // avoid explict synchronization in warp0 && unroll loop
+                break;
+            case 8:
+                runReduceSumV8<scalar_t, numel>();  // avoid explict synchronization in warp0 && unroll loop && multi add
+                break;
+            case 9:
+                runReduceSumV9<scalar_t, numel>();  // block reduce
+                break;
         }
     }
     return 0;
